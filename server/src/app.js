2c1ba46a5885c39b02f20251eb3607236224c684
jonaydan <jonaydan.rico@protonmail.com>
2026-01-26 11:42:26 +0100

    feat: sauvegarde modifications locales avant fusion équipe

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,14 @@
 import express from "express";
-
 import routes from "./routes/base.route.js";
 import challengeRoutes from "./routes/challenge.route.js";
-
-
+//import gameRoute from './routes/gameRoute';
+//import participationRoute from './routes/participationRoute';
+//import roleRoute from './routes/roleRoute';
+//import userRoute from './routes/userRoute';
 
 
 
 const app = express();
-
 // Parsing = req.body (JSON)
 app.use(express.json());
 
@@ -18,12 +18,14 @@ app.use(express.json());
 app.use("/api", routes);
 app.use("/api/challenges", challengeRoutes);
 
+//app.use('/games', gameRoute);
+//app.use('/participation', participationRoute);
+//app.use('/roles', roleRoute);
+//app.use('/users', userRoute);
 
 
 
-
-
-// errorHandler 
+// errorHandler
 
 
 export default app;
